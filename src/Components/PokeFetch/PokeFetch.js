02534a36f9bfb3cc1712b6cdeaf1abfffedf12c5
chels_s <clsextonc734@gmail.com>
2021-06-25 17:42:07 -0400

    added timer. created fade in for image and text

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './PokeFetch.css';
 
 
+
 class PokeFetch extends Component {
   constructor() {
     super()
@@ -9,6 +10,8 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      visible: false,
+      timer: 10
     }
   }
 
@@ -25,15 +28,69 @@ class PokeFetch extends Component {
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
         })
+        setInterval(() => this.tick(), 1000);
+        this.resetTimer()
       })
       .catch((err) => console.log(err))
-  }
+    }
+
+
+    tick(){
+      if (this.state.timer > 0){
+        this.setState({
+            timer: this.state.timer - 1
+        })
+      }
+      if(this.state.timer === 0){
+        clearInterval(this.state.timer)
+      }
+    }
+
+    resetTimer(){
+      if (this.state.timer === 0){
+        this.setState({
+          timer: this.state.timer = 10
+        })
+      }
+
+    }
+
+    fadeIn(){
+
+    }
+    
+    
+    componentWillUnmount(){
+      
+    }
+
+
+  // componentDidMount(){
+  //   this.myInterval = setInterval(() => {
+  //     const{seconds} = this.state
+      
+  //     if (seconds > 0) {
+  //       this.setState(({seconds}) => ({
+  //         seconds: seconds - 1
+  //       }))
+  //     }
+  //     if(seconds === 0){
+  //       clearInterval(this.myInterval)
+  //     }
+  //   }, 1000)
+  // }
+
+  // componentWillUnmount(){
+  //   clearInterval(this.myInterval)
+  // }
+ 
+  
 
   render() {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <h1 className={'timer'} >{this.state.timer}</h1>
         <div className={'pokeWrap'}>
           <img className={'pokeImg'} src={this.state.pokeSprite} />
           <h1 className={'pokeName'}>{this.state.pokeName}</h1>
